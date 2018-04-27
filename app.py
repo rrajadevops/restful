b1b4934cc924d7452f085918a9c76cc099ef9e4c
raja <raja@ge.com>
2018-04-27 20:21:43 +0530

    Added put method to update item

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api
+from flask_restful import Resource, Api, reqparse
 from flask_jwt import JWT, jwt_required
 
 from security import authenticate, identity
@@ -25,6 +25,25 @@ class Item(Resource):
         item = {'name': name, 'price': data['price']}
         items.append(item)
         return item, 201
+
+    def put(self, name):
+        parser = reqparse.RequestParser()
+        parser.add_argument('price',
+                            type=float,
+                            required=True,
+                            help="This filed is can't empty!"
+                            )
+        data = parser.parse_args()
+        #print (data['name'])
+
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        if item == None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        else:
+            item.update(data)
+        return item
+
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
